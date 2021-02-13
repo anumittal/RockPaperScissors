7f99f69e86df062b1ed06fa559a9accc10f83de4
Anu Mittal <anu22mittal@gmail.com>
2021-02-13 16:29:12 +0800

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Anu Mittal on 13/02/21.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
